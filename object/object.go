207fbe3fe609c3a48c65bc4114963f1b0304ea2b
RashikAnsar <sk.ansar46@gmail.com>
2018-12-25 17:56:56 +0530

    Foundation of Object System(Null)

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ type ObjectType string
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 // Object interface with Type and Inspect Methods
@@ -25,9 +26,16 @@ type Integer struct {
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
+// Boolean type of monkey language
 type Boolean struct {
 	Value bool
 }
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+// NULL type of monkey language
+type NULL struct{}
+
+func (n *NULL) Type() ObjectType { return NULL_OBJ }
+func (n *NULL) Inspect() string  { return "null" }

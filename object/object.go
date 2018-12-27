edca77a99b3f2c087eb6071dd08f06a17d9c1b01
RashikAnsar <sk.ansar46@gmail.com>
2018-12-28 00:26:33 +0530

    Evaluation of Custom data type(STRING)

--- object/object.go
+++ object/object.go
@@ -16,6 +16,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
+	STRING_OBJ       = "STRING"
 )
 
 // Object interface with Type and Inspect Methods
@@ -84,3 +85,10 @@ func (f *Function) Inspect() string {
 
 	return out.String()
 }
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }

e7828538cbd6b16a3554100c5822849bed7897d3
RashikAnsar <sk.ansar46@gmail.com>
2019-01-07 21:05:35 +0530

    First bytecode instruction

---
+++ code/code.go
@@ -0,0 +1,64 @@
+package code
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+// Instructions type: is a slice of bytes
+type Instructions []byte
+
+// Opcode type: is a byte
+type Opcode byte
+
+const (
+	// OpConstant to retrieves  the constant using the operand
+	OpConstant Opcode = iota
+)
+
+// Definition for an Opcode
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+}
+
+// Lookup the definition
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definitions[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op)
+	}
+
+	return def, nil
+}
+
+// Make to easily build up bytecode instructions
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definitions[op]
+	if !ok {
+		return []byte{}
+	}
+
+	instructionLen := 1
+	for _, w := range def.OperandWidths {
+		instructionLen += w
+	}
+
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	offset := 1
+	for i, o := range operands {
+		width := def.OperandWidths[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		}
+		offset += width
+	}
+	return instruction
+}
